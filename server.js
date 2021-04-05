85033db0aae6f012bd578de66d8325f485cceb1c
abuzahra98 <dawoodbasher@gmail.com>
2021-04-05 17:21:30 +0300

    api

--- server.js
+++ server.js
@@ -6,61 +6,117 @@ require('dotenv').config();
 const cors = require('cors');
 const server = express();
 const PORT = process.env.PORT || 5000;
+const superagent = require('superagent');
 
 server.use(cors());
+
 server.get('/', rr);
 server.get('/location',locatine);
 server.get('/weather',weather);
+server.get('/parks',park);
+
+server.get('*',all);
 function  rr(req,res){
     res.send('you server is working')
 }
 
 
+function Location(cityName,geoData) {
+
+    this.search_query = cityName ;
+    this.formatted_query = geoData[0].display_name;
+     this.latitude = geoData[0].lat;
+     this.longitude = geoData[0].lon;
+ }
+
+
 function locatine(req,res){
-    // let Data = require('./data/location.json');
+    let cityName = req.query.city;
+    let key = process.env.LOCATION_KEY;
+    let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+    superagent.get(LocURL) 
+    .then(geoData => {
+        let gData = geoData.body;
+        const locationData = new Location(cityName, gData);
+        res.send(locationData);
+    })
 
-    let locationData = new Location (Data);
-    res.send(locationData);
 }
 
-function Location(locData) {
 
-   this.search_query = 'Lynnwood';
-   this.formatted_query = locData[0].display_name;
-    this.latitude = locData[0].lat;
-    this.longitude = locData[0].lon;
+function Weather(gData) {
+
+    this.forecast = gData.weather.description;
+    this.time = gData.valid_date;
 }
+
 
 function weather(req,res){
-    let data=[];
-    let Data = require('./data/weather.json');
- Data.data.map(el=>
-    {
-     data.push(new Weather (el));
+    let cityName = req.query.search_query;
+    console.log(cityName)
+    let key = process.env.WEATHER_KEY;
+    let LocURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${key}`;
+
+    superagent.get(LocURL) 
+     .then(geoData => {
+        let gData = geoData.body;
+        // console.log('ddddddddddddddddddddd',gData)
+
+
+        let data=[];
+            gData.data.map(el=>
+            {
+             data.push(new Weather (el));
+            })
+        res.send(data);
     })
-    
-    res.send(data);
-}
-
-
-function Weather(locData) {
-
-console.log(locData)
-    this.forecast = locData.weather.description;
-    this.time = locData.valid_date;
 }
 
 
 
+//     let LocURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${key}`;
 
-server.get('*',(req,res)=>{
-    
+function park(req,res){
+    let cityName = req.query.search_query;
+    console.log(cityName)
+    let key = process.env.PARK_KEY;
+    let LocURL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
+    // console.log('ddddddddddddddddddddd',req.query)
+    superagent.get(LocURL) 
+     .then(geoData => {
+        let gData = geoData.body;
+       
+
+
+        let dataPark=[];
+            gData.data.map(el=>
+            {
+                dataPark.push(new Park (el));
+            })
+            console.log('ddddddddddddddddddddd',dataPark)
+        res.send(dataPark);
+    })
+}
+function Park(gData) {
+
+    this.name = gData.name;
+    this.address = gData.address;
+    this.fee=gData.fee;
+    this.description=gData.description;
+    this.url=gData.url;
+}
+
+
+
+
+
+function all(req,res){
     let err = {
         status: 500,
         responseText: "Sorry, something went wrong"
     }
     res.status(500).send(err);
-})
+}
 
 server.listen(PORT,()=>{
     console.log(`Listening on PORT ${PORT}`)
