c357aa59c75cf2b049d95bb33bf68c2501944878
abuzahra98 <dawoodbasher@gmail.com>
2021-04-09 12:50:54 +0300

    edit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Project Name
 
-**Author**: Your Name Goes Here
-**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+**Author**: dawood abuzahra
+**Version**: 1.0.0
 
 ## Overview
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
@@ -20,3 +20,143 @@
 ## Credits and Collaborations
 <!-- Give credit (and a link) to other people or resources that helped you build this application. -->
 -->
+
+Number and name of feature: #1 Repository Set Up
+
+Estimate of time needed to complete: 10 min
+
+Start time: 11: 45
+
+Finish time: 11:55
+
+Actual time needed to complete: 10 min
+
+Number and name of feature: #2 Location
+
+Estimate of time needed to complete: 15 min
+
+Start time: 12:30
+
+Finish time: 12:45
+
+Actual time needed to complete: 15 min
+
+Number and name of feature: #3 Weather
+
+Estimate of time needed to complete: 10 min
+
+Start time: 12:45
+
+Finish time: 12:55
+
+Actual time needed to complete: 10 min
+
+Number and name of feature: #4 Errors
+
+Estimate of time needed to complete: 5 min
+
+Start time: 12::55
+
+Finish time: 01:00
+
+Actual time needed to complete: 5 min
+
+Number and name of feature: #5 Data Formatting
+
+Estimate of time needed to complete: 5 min
+
+Start time: 10:20
+
+Finish time: 10:23
+
+Actual time needed to complete: 3 min
+
+Number and name of feature: #6 Locations
+
+Estimate of time needed to complete: 15 min
+
+Start time: 10:25
+
+Finish time: 10:50
+
+Actual time needed to complete: 25 min
+
+Number and name of feature: #7 Weather
+
+Estimate of time needed to complete: 20 min
+
+Start time: 11:05
+
+Finish time: 11:30
+
+Actual time needed to complete: 25 min
+
+Number and name of feature: #8 Parks
+
+Estimate of time needed to complete: 20 min
+
+Start time: 11:40
+
+Finish time: 12:20
+
+Actual time needed to complete: 20 min
+
+Number and name of feature: #9 Database setup and table creation
+
+Estimate of time needed to complete: 5 min
+
+Start time: 10:00
+
+Finish time:10:10
+
+Actual time needed to complete: 10 min
+
+Number and name of feature: #10 Persistance of location
+
+Estimate of time needed to complete: 30 min
+
+Start time: 10:20
+
+Finish time: 11:00
+
+Actual time needed to complete: 40 min
+
+Number and name of feature: #11 Movies
+
+Estimate of time needed to complete: 20 min
+
+Start time: 10:40
+
+Finish time: 11:45
+
+Actual time needed to complete: 1 hour
+
+Number and name of feature: #12 Yelp
+
+Estimate of time needed to complete: 20 min
+
+Start time: 11:50
+
+Finish time: 12:05
+
+Actual time needed to complete: 15 min
+
+Number and name of feature: #12 Pagination
+
+Estimate of time needed to complete: 15 min
+
+Start time: 12:15
+
+Finish time: 12:20
+
+Actual time needed to complete: 5 min
+
+Number and name of feature: #11 Deployment
+
+Estimate of time needed to complete: 10 min
+
+Start time: 12:25
+
+Finish time: 12:30
+
+Actual time needed to complete: 5 min
--- server.js
+++ server.js
@@ -1,154 +1,134 @@
 'use strict';
 
-const express = require('express'); 
-require('dotenv').config(); 
+const express = require('express');
+require('dotenv').config();
 const pg = require('pg');
 const cors = require('cors');
 const server = express();
 const PORT = process.env.PORT || 5000;
 const superagent = require('superagent');
-// const client = new pg.Client(process.env.DATABASE_URL);
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 server.use(cors());
 
 server.get('/', rr);
-server.get('/location',locatine);
-server.get('/weather',weather);
-server.get('/parks',park);
-server.get('/movies',movies);
-server.get('/yelp',yelp);
+server.get('/location', locatine);
+server.get('/weather', weather);
+server.get('/parks', park);
+server.get('/movies', movies);
+server.get('/yelp', yelp);
 
-server.get('*',all);
-function  rr(req,res){
+server.get('*', all);
+function rr(req, res) {
     res.send('you server is working')
 }
 
 
-function Location(cityName,geoData) {
+function Location(cityName, geoData) {
 
-    this.search_query = cityName ;
+    this.search_query = cityName;
     this.formatted_query = geoData[0].display_name;
-     this.latitude = geoData[0].lat;
-     this.longitude = geoData[0].lon;
-     this.addToDataBase=function()
-     {
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
+    this.addToDataBase = function () {
         let SQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4) RETURNING *;`;
-        let safeValues = [this.search_query,this.formatted_query,this.latitude,this.longitude];
-        client.query(SQL,safeValues)
-        .then(result=>{
-            return (result.rows);
-        })
-        .catch(error=>{
-            return (error);
-        })
-     }
- }
+        let safeValues = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+        client.query(SQL, safeValues)
+            .then(result => {
+                return (result.rows);
+            })
+            .catch(error => {
+                return (error);
+            })
+    }
+}
 
 
-function locatine(req,res){
+function locatine(req, res) {
     let cityName = req.query.city;
     const queryDataBase = 'SELECT * FROM locations WHERE search_query = $1';
     const valuesDataBase = [req.query.city];
-  
-    client.query(queryDataBase,valuesDataBase)
-    .then (result=>{
-       if(result.rows.length>0)
-       {
-        res.send(result.rows[0]);
-console.log(11111111111)
-       }else
-       {
-        console.log(22222222)
-              let key = process.env.LOCATION_KEY;
-    let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
-    superagent.get(LocURL) 
-    .then(geoData => {
-        
-        let gData = geoData.body;
-   
-        const locationData = new Location(cityName, gData);
-        locationData.addToDataBase();
-        res.send(locationData);
-    })
-       }
-        
-       
-    })
-    .catch(error=>{
-        res.send(error);
-    })
+
+    client.query(queryDataBase, valuesDataBase)
+        .then(result => {
+            if (result.rows.length > 0) {
+                res.send(result.rows[0]);
+                console.log(11111111111)
+            } else {
+                console.log(22222222)
+                let key = process.env.LOCATION_KEY;
+                let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+                superagent.get(LocURL)
+                    .then(geoData => {
+
+                        let gData = geoData.body;
+
+                        const locationData = new Location(cityName, gData);
+                        locationData.addToDataBase();
+                        res.send(locationData);
+                    })
+            }
+
+
+        })
+        .catch(error => {
+            res.send(error);
+        })
 
 }
 
 
-function Weather(gData) {
-    this.forecast = gData.weather.description;
-    this.time = gData.valid_date;
-}
 
 
-function weather(req,res){
+
+function weather(req, res) {
     let cityName = req.query.search_query;
     // console.log(cityName)
     let key = process.env.WEATHER_KEY;
     let LocURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${key}`;
 
-    superagent.get(LocURL) 
-     .then(geoData => {
-        let gData = geoData.body;
-        let data=[];
-            gData.data.map(el=>
-            {
-             data.push(new Weather (el));
+    superagent.get(LocURL)
+        .then(geoData => {
+            let gData = geoData.body;
+            let data = [];
+            gData.data.map(el => {
+                data.push(new Weather(el));
             })
-        res.send(data);
-    })
+            res.send(data);
+        })
+}
+function Weather(gData) {
+    this.forecast = gData.weather.description;
+    this.time = gData.valid_date;
 }
 
-function park(req,res){
-    let cityName = req.query.search_query;
-    let key = process.env.PARK_KEY;
-    let LocURL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
-    superagent.get(LocURL) 
-     .then(geoData => {
-        let gData = geoData.body;
-    
-        let dataPark=[];
-            gData.data.map(el=>
-            {
-                dataPark.push(new Park (el));
-            })
-        res.send(dataPark);
-    })
-
-
-}
 function Park(gData) {
 
     this.name = gData.name;
     this.address = gData.address;
-    this.fee=gData.fee;
-    this.description=gData.description;
-    this.url=gData.url;
+    this.fee = gData.fee;
+    this.description = gData.description;
+    this.url = gData.url;
 }
-function movies (req,res){
-    let cityName = req.query.search_query; 
-    console.log(req.query.search_query)
-    let key = process.env.MOVIES;
-    let LocURL = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
-superagent.get(LocURL)
-.then(geoData => {
-    let gData = geoData.body.results;
+function park(req, res) {
+    let cityName = req.query.search_query;
+    let key = process.env.PARK_KEY;
+    let LocURL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
+    superagent.get(LocURL)
+        .then(geoData => {
+            let gData = geoData.body;
 
-    let datamovies=[];
-        gData.map(el=>
-        {
-            datamovies.push(new Movies (el));
+            let dataPark = [];
+            gData.data.map(el => {
+                dataPark.push(new Park(el));
+            })
+            res.send(dataPark);
         })
-    res.send(datamovies);
-})
+
+
 }
+
 
 function Movies(gData) {
 
@@ -160,47 +140,63 @@ function Movies(gData) {
     this.popularity = gData.popularity;
     this.released_on = gData.release_date;
 }
-
-function yelp (req,res){
-    let cityName = req.query.search_query; 
+function movies(req, res) {
+    let cityName = req.query.search_query;
     console.log(req.query.search_query)
-    let key = process.env.YELP;
-    const numberOfbage = 5;
-    const start = ((req.query.page - 1) * numberOfbage+1);
-    let LocURL = `https://api.yelp.com/v3/businesses/search?term=restaurant&location=${cityName}&limit=${numberOfbage}&offset=${start}`;
-superagent.get(LocURL)
-.set('Authorization', `Bearer ${key}`)
-.then(geoData => {
-    let gData = geoData.body.businesses;
+    let key = process.env.MOVIES;
+    let LocURL = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
+    superagent.get(LocURL)
+        .then(geoData => {
+            let gData = geoData.body.results;
 
-    let datayelp=[];
-        gData.map(el=>
-        {
-            datayelp.push(new Yelp (el));
+            let datamovies = [];
+            gData.map(el => {
+                datamovies.push(new Movies(el));
+            })
+            res.send(datamovies);
         })
-    res.send(datayelp);
-})
 }
 
 function Yelp(gData) {
 
     this.name = gData.name;
-  this.image_url = gData.image_url;
-  this.price = gData.price;
-  this.rating = gData.rating;
-  this.url = gData.url;
+    this.image_url = gData.image_url;
+    this.price = gData.price;
+    this.rating = gData.rating;
+    this.url = gData.url;
+}
+function yelp(req, res) {
+    let cityName = req.query.search_query;
+    console.log(req.query.search_query)
+    let key = process.env.YELP;
+    const numberOfbage = 5;
+    const start = ((req.query.page - 1) * numberOfbage + 1);
+    let LocURL = `https://api.yelp.com/v3/businesses/search?term=restaurant&location=${cityName}&limit=${numberOfbage}&offset=${start}`;
+    superagent.get(LocURL)
+        .set('Authorization', `Bearer ${key}`)
+        .then(geoData => {
+            let gData = geoData.body.businesses;
+
+            let datayelp = [];
+            gData.map(el => {
+                datayelp.push(new Yelp(el));
+            })
+            res.send(datayelp);
+        })
+}
+
+function Yelp(gData) {
+
+    this.name = gData.name;
+    this.image_url = gData.image_url;
+    this.price = gData.price;
+    this.rating = gData.rating;
+    this.url = gData.url;
 }
 
 
 
-
-
-
-
-
-
-
-function all(req,res){
+function all(req, res) {
     let err = {
         status: 500,
         responseText: "Sorry, something went wrong"
